a93c5366be3edb9b1b37217beb4dd7ba37d4e88a
Alvin Ramoutar <alvinramoutar@gmail.com>
2022-07-20 20:03:18 -0400

    format pattern_set_test

--- test/pattern_set_test.dart
+++ test/pattern_set_test.dart
@@ -5,10 +5,13 @@ import 'package:sudoku_api/sudoku_api.dart';
 void main() async {
   String patterSetTest = 'Error Pattern Set';
   test(patterSetTest, () {
-    final Map<int, String> errorMap = {0: "0 0 0 0 1 0 0 0 0 1 0",};
-    final patterSet= PatternSet();
-    final patterName="errorPattern";
-    Pattern pattern= Pattern(patterName, errorMap);
-    expect(() => patterSet.add(pattern), throwsA(isA<InvalidPatternException>()));
+    final Map<int, String> errorMap = {
+      0: "0 0 0 0 1 0 0 0 0 1 0",
+    };
+    final patterSet = PatternSet();
+    final patterName = "errorPattern";
+    Pattern pattern = Pattern(patterName, errorMap);
+    expect(
+        () => patterSet.add(pattern), throwsA(isA<InvalidPatternException>()));
   });
 }
